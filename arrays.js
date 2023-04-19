35e76a962ceaa705a63ab7ddfaaa47034135415b
Judy Ye <judyy.ye@gmail.com>
2023-04-19 12:37:07 -0700

    add three Array methods and notes

--- arrays.js
+++ arrays.js
@@ -1,4 +1,12 @@
 var names = ["Jenny", "Jerry", "John", "JJ"];
 var ages = [4, 5, 6, 7]
-var isConfused = [true, true, true, false]
+var isConfused = [false, true, true, false]
 
+// removes the last element, "JJ" in array
+names.pop()
+
+// adds the Number, 100, to the end of the array
+ages.push(100)
+
+// adds the Boolean, true, to the beginning of array
+isConfused.unshift(true)
