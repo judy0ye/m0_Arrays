8bc564df17ed8bb2f406d84e2fa96c6ac2608928
Judy Ye <judyy.ye@gmail.com>
2023-04-19 12:41:55 -0700

    add notes and demonstrate understanding of index positions

--- arrays.js
+++ arrays.js
@@ -10,3 +10,11 @@ ages.push(100)
 
 // adds the Boolean, true, to the beginning of array
 isConfused.unshift(true)
+
+
+// index position starts at 0
+// for example, the line of code below will print
+// the Number 4 to the console instead of of the 
+// Number 2 since the element position is at 1
+var ages = [2, 4, 6, 8, 10]
+console.log(ages[1]);
